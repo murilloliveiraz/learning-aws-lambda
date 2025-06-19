6fe0cda078329f5b8f06fae87965a0e6fcaee4f1
Murillo Oliveira <murillo1047@gmail.com>
2025-06-19 16:36:18 -0300

    feat: searching objects in s3

--- index.mjs
+++ index.mjs
@@ -1,14 +1,30 @@
 import { log } from "./log.mjs";
+import { S3 } from "@aws-sdk/client-s3";
+
+const s3Client = new S3({ region: 'us-east-1' })
 
 export const handler = async (event) => {
-  log('event: ' + JSON.stringify(event))
-  return {
-    statusCode: 200,
-    body: `<html>
-                <body>
-                    <h1>Dados da Requisicao ${JSON.stringify(event)}</h1>
-                </body>
-            </html>`,
-    headers: {"content-type": "text/html"}
-  };
+  let record;
+  let Bucket;
+  let Key;
+  if(event.Records[0]){
+    record = event.Records[0];
+    bucket = event.s3.bucket.name;
+    bucket = event.s3.object.key;
+  }
+
+  const getObjectResult = await s3Client.getObject({
+      Bucket,
+      Key,
+  });
+
+  const mega_byte = 1024 * 1024;
+
+  if(getObjectResult.ContentLength > 1 * mega_byte){
+    log('Objeto muito grande');
+    return 'Objeto muito grande';
+  }
+  
+  log('Objeto de tamanho OK');
+  return 'Objeto de tamanho OK';
 };
